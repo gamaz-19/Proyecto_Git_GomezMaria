387bc5f93eb7584bfa6e3199dc7e023f07b8838c
Maria Gomez <maga1731@gmail.com>
2025-06-03 13:16:24 -0500

    feat: :tada: Starting git project, creating main document

---
+++ main.py
@@ -0,0 +1,56 @@
+###################################
+####### Empanadas Doña Pepa #######
+###################################
+
+
+
+
+
+
+
+
+
+
+booleano = True
+while booleano
+
+
+print ('''
+Bienvenido a Empanadas Doña Pepa
+
+1. Tipos de empanadas
+2. Registrar empanada
+3. Editar empanadas
+4. Eliminar empanadas
+''')
+empanadasTodas= []
+
+menuPrincipal = (int(input("Ingrese el numero de su seleccion: ")))
+    
+    if (menuPrincipal==1)
+        print("Tipos de empanadas")
+
+    elif (menuPrincipal==2)
+        print("Registrar empanada")
+        nombre= (input("Nombre = "))
+        precio= (input("Precio = "))
+        ingredientes= (input("Ingredientes = "))
+        disponibilidad= (input("Disponibilidad = "))
+
+        empanadaNueva={
+            "nombre" : nombre,
+            "precio" : precio,
+            "ingredientes" : ingredientes,
+            "disponibilidad" : disponibilidad
+        }
+
+    elif (menuPrincipal==3)
+        print("Editar empanada")
+        
+
+
+    elif (menuPrincipal==4)
+        print("Eliminar empanada")
+
+
+# Desarrollado por: Maria Alejandra Gomez Archila - cc.1005234916
